4dfd00cd0e73287f917d5d7afd8a982a4eb05d48
@hmb_mirzaei <hmb.mirzaei@gmail.com>
2024-02-21 22:11:38 +0330

    postman doc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { config } = require('dotenv');
 config();
 const { json } = express;
-const { log, not_found } = require('./controller/utility');
+const { log, not_found, err } = require('./controller/utility');
 const app = express();
 app.use(json());
 app.use('/*', log);
@@ -18,14 +18,28 @@ const xss = require('xss-clean');
 app.use(xss());
 // ------ hpp & xss ------
 
-// ------ hpp & xss ------
+// ------ postman ------
 const postman = require('./controller/postman');
 app.get('/postman', (r, s) => s.json(postman()));
-// ------ hpp & xss ------
+// ------ postman ------
 
+// ------ 404 ------
 app.use('/*', not_found);
+// ------ 404 ------
+
 const port = process.env.port || 8000;
 app.listen(port, _ => {
     console.log(`listening on port: ${port}`);
     console.log(`http://localhost:${port}/postman`);
 });
+
+process
+    .on('unhandledRejection', (reason, p) => {
+        console.error(reason, 'Unhandled Rejection at Promise', p);
+    })
+    .on('uncaughtException', err => {
+        console.error(err, 'Uncaught Exception thrown');
+        process.exit(1);
+    })
+
+
--- route_controller/auth.js
+++ route_controller/auth.js
@@ -14,4 +14,4 @@ const funcs = {
 		};
 	}
 }
-module.exports = funcs
+module.exports = funcs;
